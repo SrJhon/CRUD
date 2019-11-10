efdb6f9f2ee5f8088a27104ae6d82950b129a3a3
SrJhon <36854243+SrJhon@users.noreply.github.com>
2019-11-09 22:38:00 -0500

    Update conexion.php

--- resources/database/conexion.php
+++ resources/database/conexion.php
@@ -4,5 +4,9 @@
 $conectar = mysqli_connect("localhost","root", "Huila.3218109199", "registro_notas");
 // Check connection
 if (!$conectar) {
-      die("Connection failed: " . mysqli_connect_error());
+      echo 'Error al conectar a la base de datos';
+}else{
+ 
+      echo 'Conectado a la base de datos';
+      
 }
