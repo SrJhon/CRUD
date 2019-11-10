700ec8540751ee660941caad33ffbe177ad1b5ba
SrJhon <36854243+SrJhon@users.noreply.github.com>
2019-11-09 22:55:16 -0500

    Update conexion.php

--- resources/database/conexion.php
+++ resources/database/conexion.php
@@ -7,6 +7,6 @@ if (!$conectar) {
       echo 'Error al conectar a la base de datos';
 }else{
  
-      echo 'Conectado a la base de datos';
+      echo 'Conectado a la base de datos '.$conectar.' <----- datos - conectar';
       
 }
